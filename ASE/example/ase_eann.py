34faa6763ea789fa8ef6ac521e49e7a898f92e02
chen-jialan <jlchen20@mail.ustc.edu.cn>
2022-03-10 12:41:49 +0800

    ”update_example”

---
+++ ASE/example/ase_eann.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Nov 28 15:52:34 2021
+
+@author: DELL
+"""
+
+import ase.io.vasp
+from ase import Atoms
+from ase.calculators.eann import EANN
+from ase.calculators.reann import REANN
+#import os
+#import re
+from ase.optimize.minimahopping import MinimaHopping
+from ase.optimize.minimahopping import MHPlot
+from ase.optimize import LBFGS
+from ase.io.trajectory import Trajectory
+import pickle
+import torch
+import numpy as np
+
+cell1 = ase.io.vasp.read_vasp("POSCAR")
+atoms = Atoms(cell1)
+#--------------the type of atom, which is the same atomtype as para/input_density--------------
+atomtype = ['Pt','Fe','O']
+#atomtype = ['Cu','Ce','O','C']
+#-----------------the device is cpu or gpu( cpu is default)---------------
+device='cpu'
+#-------------------------pbc([1,1,1] is default)---------------------
+period=[1,1,1]
+#---------------nn file('EANN_PES_DOUBLE.pt' is default)----------------------------
+nn = 'REANN_PES_DOUBLE.pt'
+#----------------------eann --------------------------------
+atoms.calc = REANN(device=device,atomtype=atomtype,period=period,nn=nn)
+#atoms.calc = EANN()
+#----------------------------reann---------------------------------
+#atoms.calc = REANN(atomtype=atomtype,period=[1,1,0],nn = 'EANN_PES_DOUBLE.pt')
+#print(atoms.get_potential_energy())
+dyn = LBFGS(atoms,trajectory='atom2.traj')
+dyn.run(fmax=0.2,steps=100)
+traj = Trajectory('atom2.traj')
+atoms = traj[-1]
+ase.io.write('POSCAR-final', atoms, format='vasp', vasp5='True')
+f = atoms.get_forces()
+print(np.max(f))
